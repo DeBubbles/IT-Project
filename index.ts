f78f58cbc9f401f0be567d343cd08a4177f3c606
Nicolas <nicolas.champagne@student.ap.be>
2022-03-27 21:05:46 +0200

    Start API

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
 
 app.set("port", 4200);
@@ -10,12 +11,32 @@ app.listen(app.get("port"), () =>
 
 app.use(express.static(__dirname + "/views"));
 
-app.get("/", (req: any, res: any) => {
+app.get("/", async function (req: any, res: any) {
+  const api = await axios.get(`https://api.magicthegathering.io/v1/cards`);
+  const data = api.data;
+
+  let images: any[] = [];
+  for (let index = 0; index < data.cards.length; index++) {
+    for (let [key, value] of Object.entries(data.cards[index])) {
+      if (key == "imageUrl") {
+        images.push(value);
+      }
+    }
+  }
+  console.log(images);
+
   res.type("text/html");
-  res.render("index");
+  res.render("index", {
+    image: images,
+  });
 });
 
 app.get("/decks", (req: any, res: any) => {
   res.type("text/html");
   res.render("decks");
+});
+
+app.get("/draw", (req: any, res: any) => {
+  res.type("text/html");
+  res.render("draw");
 });
