205ce5a1991e1efe081187a875bd277f55797458
Nicolas <nicolas.champagne@student.ap.be>
2022-04-28 22:32:01 +0200

    onClick

--- index.ts
+++ index.ts
@@ -19,15 +19,27 @@ app.get("/", (req: any, res: any) => {
 });
 
 app.get("/mtg", async function (req: any, res: any) {
-  const api = await axios.get(`https://api.magicthegathering.io/v1/cards`);
-  const data = api.data;
-
   let images: any[] = [];
+  let text: boolean = false;
+  for (let index = 1; index < 3; index++) {
+    const api = await axios.get(
+      `https://api.magicthegathering.io/v1/cards?page=${index}`
+    );
+    const data = api.data;
 
-  for (let index = 0; index < data.cards.length; index++) {
-    for (let [key, value] of Object.entries(data.cards[index])) {
-      if (key == "imageUrl") {
-        images.push(value);
+    for (let index = 0; index < data.cards.length; index++) {
+      for (let [key, value] of Object.entries(data.cards[index])) {
+        if (key == "imageUrl") {
+          let name = data.cards[index].name;
+          let urlImage = value;
+
+          let kaart = {
+            name: name,
+            url: urlImage,
+          };
+
+          images.push(kaart);
+        }
       }
     }
   }
@@ -35,38 +47,37 @@ app.get("/mtg", async function (req: any, res: any) {
   res.type("text/html");
   res.render("home", {
     image: images,
+    tekst: text,
   });
 });
 
 app.get("/mtg/zoeken", async function (req: any, res: any) {
-  let search = req.body.search;
+  let search = req.query.search;
 
   const api = await axios.get(`https://api.magicthegathering.io/v1/cards`);
   const data = api.data;
 
   let images: any[] = [];
+  let text: boolean = false;
 
   for (let index = 0; index < data.cards.length; index++) {
     let values = Object.values(data.cards[index]);
     let keys = Object.keys(data.cards[index]);
-
-    //Foreach
-    //boolean
-
     if (values.includes(search) || keys.includes(search)) {
-      for (let [key, value] of Object.entries(data.cards[index])) {
-        if (key == "imageUrl") {
-          images.push(value);
-        }
+      if (data.cards[index].imageUrl != null) {
+        images.push(data.cards[index].imageUrl);
       }
-    } else {
-      continue;
+    }
+
+    if (images.length == 0) {
+      text = true;
     }
   }
 
   res.type("text/html");
   res.render("home", {
     image: images,
+    tekst: text,
   });
 });
 
@@ -79,3 +90,7 @@ app.get("/mtg/draw", (req: any, res: any) => {
   res.type("text/html");
   res.render("draw");
 });
+
+function onClick(_src: string) {
+  alert(_src);
+}
